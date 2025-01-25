a8cfc06e0e761f13b80ed0bc4479655442491c51
Filip Ivanusec <fivanusec@gmail.com>
2025-01-25 15:07:53 +0100

    feat(node): handling of json node

---
+++ include/Node.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace JSON {
+class JSON_Node;
+using JSON_Object = std::map<std::string, std::shared_ptr<JSON_Node>>;
+using JSON_List = std::vector<std::shared_ptr<JSON_Node>>;
+
+enum Type { OBJECT, LIST, STRING, NUMBER, BOOLEAN, NULL_TYPE };
+union Values {
+  JSON_Object *object;
+  JSON_List *list;
+  std::string *s;
+  float f_value;
+  bool b_value;
+};
+
+class JSON_Node {
+private:
+  Type type;
+  Values values;
+
+public:
+  JSON_Node() {}
+  JSON_Node(const Type &type) : type(type) {}
+
+  auto get_object() {
+    if (type == Type::OBJECT)
+      return *this->values.object;
+    throw std::logic_error("Improper return");
+  }
+
+  auto get_list() {
+    if (type == Type::LIST)
+      return *this->values.list;
+    throw std::logic_error("Improper return");
+  }
+
+  auto get_string() {
+    if (type == Type::STRING)
+      return *this->values.s;
+    throw std::logic_error("Improper return");
+  }
+
+  auto get_number() {
+    if (type == Type::NUMBER)
+      return this->values.f_value;
+    throw std::logic_error("Improper return");
+  }
+
+  void set_object(JSON_Object *object) {
+    this->values.object = object;
+    type = Type::OBJECT;
+  }
+
+  void set_list(JSON_List *list) {
+    this->values.list = list;
+    type = Type::LIST;
+  }
+
+  void set_string(std::string *s) {
+    this->values.s = s;
+    type = Type::STRING;
+  }
+
+  void print_node(int indentation);
+  std::string to_string(int indentation);
+};
+} // namespace JSON
---
+++ src/node.cpp
@@ -0,0 +1,51 @@
+#include "Node.h"
+#include <iostream>
+#include <string>
+
+std::string JSON::JSON_Node::to_string(int indentation) {
+  std::string space_string = std::string(indentation, ' ');
+  std::string output_string = "";
+
+  switch (type) {
+  case JSON::Type::STRING:
+    output_string += space_string + *values.s;
+    break;
+  case JSON::Type::NUMBER:
+    output_string += space_string + std::to_string(values.f_value);
+    break;
+  case JSON::Type::LIST: {
+
+    int index = 0;
+    for (auto node : (*values.list)) {
+      output_string += node->to_string(indentation + 1);
+      if (index < (*values.list).size() - 1) {
+        output_string += space_string + ", ";
+      }
+      index++;
+    }
+    output_string += space_string + "]\n";
+    break;
+  }
+  case JSON::Type::OBJECT: {
+    output_string += "{\n";
+    for (JSON::JSON_Object::iterator i = (*values.object).begin();
+         i != (*values.object).end(); i++) {
+      output_string += space_string + i->first + ":";
+      output_string += i->second->to_string(indentation + 1);
+      JSON_Object::iterator next = i;
+      next++;
+      if ((next) != (*values.object).end()) {
+        output_string += space_string + ",";
+      }
+      output_string += space_string + "\n";
+    }
+    output_string += "}\n";
+  }
+  }
+
+  return output_string;
+}
+
+void JSON::JSON_Node::print_node(int indentation) {
+  std::cout << to_string(indentation);
+}
