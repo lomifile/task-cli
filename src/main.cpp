6a06a0220bb24f039500da58d748c2e670214f95
Filip Ivanusec <fivanusec@gmail.com>
2025-02-04 16:57:44 +0100

    test: check print function

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,8 @@
 int main() {
   Task::TaskManager *task_manager = new Task::TaskManager();
   task_manager->create_new_task(new std::string("Task 2"));
-  task_manager->print_tasks();
   task_manager->update_task(1, new std::string("Hello"));
   task_manager->print_tasks();
-
   delete task_manager;
   return 0;
 }
